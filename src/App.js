a4f8ade885154c86b083983c035dbb05a7e63a3f
Marcelo Seiji <ikuta.marcelo@gmail.com>
2022-04-30 20:35:45 -0300

    featura: Display component, first style configs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import './App.css'
-import ComponentTest from './components/ComponentTest'
+import './styles/App.css'
+import Display from './components/Display'
 
 const App = () => {
   return (
-    <div>
-      <ComponentTest />
-      <h1 className="heading">React Boilerplate</h1>
-      <h4 className="sub-heading">Now we can start out the project</h4>
+    <div className="display">
+      <Display />
     </div>
   )
 }
---
+++ src/components/Display/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './styles.css'
+
+const Display = () => {
+  return <h1 className="display__title">QUAL É O NÚMERO?</h1>
+}
+
+export default Display
