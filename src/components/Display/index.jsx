f304a83c690a353fa5a7a37042f3b554e30536f2
Marcelo Seiji <ikuta.marcelo@gmail.com>
2022-04-30 22:45:11 -0300

    feature: Display component style

--- src/components/Display/index.jsx
+++ src/components/Display/index.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './styles.css'
 
 const Display = () => {
-  return <h1 className="display__title">QUAL É O NÚMERO?</h1>
+  return (
+    <div className="display__title">
+      <h1 className="display__title__text">QUAL É O NÚMERO?</h1>
+      <hr className="display__title__line" />
+    </div>
+  )
 }
 
 export default Display
