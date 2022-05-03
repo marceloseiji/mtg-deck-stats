a8085cd1a346f32b8f79d35079c4e406331da377
Marcelo Seiji <ikuta.marcelo@gmail.com>
2022-05-02 22:11:39 -0300

    chore: build path fix

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public', 'dist')))
 app.set('port', process.env.PORT || 3000)
 
 const server = app.listen(app.get('port'), function () {
   console.log('listening on port ', server.address().port)
 })
 
-const path = require('path')
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
