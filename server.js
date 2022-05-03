029c6d68282479ba20634c1741bc2b4ec4f739b6
Marcelo Seiji <ikuta.marcelo@gmail.com>
2022-05-02 21:24:03 -0300

    chore: server.js fix to deploy

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'public/dist')))
 app.set('port', process.env.PORT || 3000)
 
 const server = app.listen(app.get('port'), function () {
   console.log('listening on port ', server.address().port)
+})
+
+const path = require('path')
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
 })
