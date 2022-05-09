14f0da7e8fc021b28180061d34726fa33c1450db
Marcelo Seiji <ikuta.marcelo@gmail.com>
2022-05-09 19:59:43 -0300

    feature: adding drawer

---
+++ src/components/Drawer/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { MtgDrawer } from './styles'
+
+const Drawer = ({ component }) => {
+  return <MtgDrawer component={component} />
+}
+
+export default Drawer
---
+++ src/pages/Main/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Drawer from 'components/Drawer'
+
+const Main = () => {
+  return <Drawer />
+}
+
+export default Main
