453d49792b7cca2a33838b16c67a2aff78d77cfb
Marcelo Seiji <ikuta.marcelo@gmail.com>
2022-05-10 17:23:37 -0300

    Revert "feature: card autocomplete added"
    This reverts commit 909a3efd3f5e717b42e60b444fa44032cab35a47.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react'
 import AppRouter from 'routers/AppRouter'
 import { GlobalProvider } from './contexts/GlobalContext'
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles'
-
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    background: {
-      default: '#1f2936',
-      paper: '#334050'
-    }
-  }
-})
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalProvider>
-        <AppRouter />
-      </GlobalProvider>
-    </ThemeProvider>
+    <GlobalProvider>
+      <AppRouter />
+    </GlobalProvider>
   )
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,47 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import Cards from 'services/Cards'
-import Autocomplete from 'components/Autocomplete'
 
 const Home = () => {
   const [fetchedCardInfos, setFetchedCardInfos] = useState('')
-  const [inputTypedValue, setInputTypedValue] = useState('')
-  const [loading, setLoading] = useState(false)
+  const [inputTyped, setInputTyped] = useState('')
 
   const getCardsByName = async () => {
-    const { data } = await Cards.withName(inputTypedValue)
-    setFetchedCardInfos(data.map((card) => ({ title: card.name })))
+    const { data } = await Cards.withName(inputTyped)
+    setFetchedCardInfos(data)
+  }
+
+  const handleInputValue = (value) => {
+    setInputTyped(value)
+  }
+
+  const testButton = (name) => {
+    console.log(name)
   }
 
   useEffect(() => {
-    if (fetchedCardInfos.length > 0) {
-      setLoading(false)
-    }
-  }, [fetchedCardInfos])
-
-  useEffect(() => {
-    if (inputTypedValue?.length > 2) {
+    if (inputTyped.length > 2) {
       let executeAfterTimeOut = setTimeout(() => {
-        getCardsByName(inputTypedValue)
+        getCardsByName(inputTyped)
       }, [1000])
-      setLoading(true)
 
       return () => {
         clearTimeout(executeAfterTimeOut)
       }
     }
-  }, [inputTypedValue])
+  }, [inputTyped])
 
   return (
     <>
-      <h1>Your decks</h1>
-      <Autocomplete
-        label={'Ender card name...'}
-        data={fetchedCardInfos}
-        setData={setFetchedCardInfos}
-        setInputTypedValue={setInputTypedValue}
-        loading={loading}
-        noOptionsText="Enter card name..."
+      <h1>Cards</h1>
+      <input
+        autoComplete="off"
+        type="text"
+        placeholder="Enter card name..."
+        value={inputTyped}
+        onChange={(event) => handleInputValue(event.target.value)}
       />
+      {fetchedCardInfos &&
+        fetchedCardInfos.map((card, index) => (
+          <span action onClick={testButton(card.name)} key={index}>
+            {card.name}
+          </span>
+        ))}
     </>
   )
 }
